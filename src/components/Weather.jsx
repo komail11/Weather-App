6cb3da0e380a08105001f044661e1ac9079bba7b
komail11 <komailahmed74@gmail.com>
2025-01-23 21:07:38 +0500

    Completed UI of weather app

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,25 +1,49 @@
 import React, { useState } from "react";
 import "../styles/Weather.css";
-import search from "../assets/search.png";
-
+import search from '../assets/search.png';
+import clear from '../assets/clear.png';
+import cloud from '../assets/cloud.png';
+import drizzle from '../assets/drizzle.png';
+import humidity from '../assets/humidity.png';
+import rain from '../assets/rain.png';
+import snow from '../assets/snow.png';
+import wind from '../assets/wind.png';
 
 const Weather = () => {
 
-    const [query, setQuery] = useState("");
-    
-    const display = (e) => {
-        setQuery(e.target.value);
-    }
-
     return (
-        <div className="weather">
-            <div className="search-bar">
-                <input type="text" onChange={display} placeholder="Search" className="search-input" />
-                <img src={search} alt="search" className="search-icon" />
+        <div>
+            <div className="weather-container">
+                <div className="weather">
+                    <div className="search-bar">
+                        <input type="text" placeholder="Search" />   
+                        <img src={search} alt="search" />
+                    </div>
+                    <img src={clear} alt="clear" className="weather-icon"></img>
+                    <p className="temperature">60°C</p>
+                    <p className="location">London</p>
+                    <div className="weather-details">
+                        <div className="col">
+                            <img src={humidity} alt="humidity" />
+                            <div>
+                                <p>80%</p>
+                                <span>Humidity</span>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <img src={wind} alt="wind" />
+                            <div>
+                                <p>15 km/h</p>
+                                <span>Wind Speed</span>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
-            <h1>{query}</h1>
+           
         </div>
-    );
+    ); 
 };
 
 export default Weather;
